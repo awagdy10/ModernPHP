e96dd880b8a962a080553252f7a4572a42de3e79
Ahmed Wagdy <ahmed.w1050@gmail.com>
2020-05-13 16:29:10 +0200

    added PHP-DI library

--- public/index.php
+++ public/index.php
@@ -3,7 +3,21 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 
-$helloWorld = new \ModernPHP\HelloWorld();
+$containerBuilder = new \DI\ContainerBuilder();
+
+// For using a smiple container
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+
+// For the use of the simple container, we have to define the injections ourselves
+$containerBuilder->addDefinitions([
+    \ModernPHP\HelloWorld::class => \DI\create(\ModernPHP\HelloWorld::class)
+]);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(\ModernPHP\HelloWorld::class);
+
 $helloWorld->announce();
 
 ?>
